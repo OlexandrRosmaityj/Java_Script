26f12e9cf888920612f75d2a0a6f959d94e80daf
OlexandrRosmaityj <rosmaityj@gmail.com>
2021-11-30 02:07:16 +0200

    end

--- Classwork/lesson7/main7.js
+++ Classwork/lesson7/main7.js
@@ -58,97 +58,98 @@
 // -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
 
 
-// class Cars2 {
-//     constructor(model, producer, year, maxspeed, volume) {
-//         this.model = model;
-//         this.producer = producer;
-//         this.year = year;
-//         this.maxspeed = maxspeed;
-//         this.volume = volume;
-//         this.drive = function () {
-//             console.log(`Їдемо зі швидкістю ${this.maxspeed} на годину`)
-//         }
-//
-//         this.info = function () {
-//             for (const key in this) {
-//                 if (typeof this[key] !== 'function')
-//                 console.log(`${key} - ${this[key]}`)
-//             }
-//         }
-//
-//         this.increaseMaxSpeed = function (newSpeed) {
-//             this.maxspeed = this.maxspeed + newSpeed;
-//         };
-//
-//         this.changeYear = function (newValue) {
-//             this.year = newValue;
-//         };
-//
-//         this.addDriver = function (driver) {
-//             this.driver = driver
-//         };
-//     }
-// }
-//
-// let addCar = new Cars2('WV', 'Detchland', 2010, 200,2 );
-// console.log(addCar);
-// addCar.drive();
-// addCar.info();
-// addCar.increaseMaxSpeed(50);
-// addCar.drive();
-// addCar.changeYear(2015)
-// addCar.info();
-// addCar.addDriver('Петро')
-// console.log(addCar)
+class Cars2 {
+    constructor(model, producer, year, maxspeed, volume) {
+        this.model = model;
+        this.producer = producer;
+        this.year = year;
+        this.maxspeed = maxspeed;
+        this.volume = volume;
+    }
+        drive()  {
+            console.log(`Їдемо зі швидкістю ${this.maxspeed} на годину`)
+        }
+
+    info () {
+            for (const key in this) {
+                if (typeof this[key] !== 'function')
+                console.log(`${key} - ${this[key]}`)
+            }
+        }
+
+     increaseMaxSpeed (newSpeed) {
+            this.maxspeed = this.maxspeed + newSpeed;
+        };
+
+        changeYear  (newValue) {
+            this.year = newValue;
+        };
+
+       addDriver (driver) {
+            this.driver = driver
+        };
+
+}
+
+let addCar = new Cars2('WV', 'Detchland', 2010, 200,2 );
+console.log(addCar);
+addCar.drive();
+addCar.info();
+addCar.increaseMaxSpeed(50);
+addCar.drive();
+addCar.changeYear(2015)
+addCar.info();
+addCar.addDriver('Петро')
+console.log(addCar)
 
 
 
 // -створити класс/функцію конструктор попелюшка з полями ім'я, вік, розмір ноги. Створити масив з 10 попелюшок.
-class Popelushka {
-    constructor(name, age, foot_size) {
-        this.name = name;
-        this.age = age;
-        this.foot_size = foot_size;
-    }
-}
+// class Popelushka {
+//     constructor(name, age, foot_size) {
+//         this.name = name;
+//         this.age = age;
+//         this.foot_size = foot_size;
+//     }
+// }
 //
-let popelushkaArr = [
-    new Popelushka('Sveta', 30, 38),
-    new Popelushka('Olja', 32, 36),
-    new Popelushka('Katja', 16, 35),
-    new Popelushka('Ira', 20, 36),
-    new Popelushka('Vika', 21, 30),
-    new Popelushka('Evgenia', 20, 45),
-    new Popelushka('Lida', 19, 39),
-    new Popelushka('Nastja', 14, 40),
-    new Popelushka('Jula', 10, 29),
-    new Popelushka('Anja', 32, 38),
-];
-console.log(popelushkaArr);
+// let popelushkaArr = [
+//     new Popelushka('Sveta', 30, 38),
+//     new Popelushka('Olja', 32, 36),
+//     new Popelushka('Katja', 16, 35),
+//     new Popelushka('Ira', 20, 36),
+//     new Popelushka('Vika', 21, 30),
+//     new Popelushka('Evgenia', 20, 45),
+//     new Popelushka('Lida', 19, 39),
+//     new Popelushka('Nastja', 14, 40),
+//     new Popelushka('Jula', 10, 29),
+//     new Popelushka('Anja', 32, 38),
+// ];
+// console.log(popelushkaArr);
 
 // Сторити об'єкт класу "принц" за допомоги класу який має поля ім'я, вік, туфелька яку він знайшов.
-
-class Princ {
-    constructor(name, age, shoe) {
-        this.name = name;
-        this.age = age;
-        this.shoe = shoe;
-    }
-}
-let men = new Princ('Vasyl', 20, 45);
-console.log(men)
+//
+// class Princ {
+//     constructor(name, age, shoe) {
+//         this.name = name;
+//         this.age = age;
+//         this.shoe = shoe;
+//     }
+// }
+// let men = new Princ('Vasyl', 20, 45);
+// console.log(men)
 
 //     За допомоги циклу знайти яка попелюшка повинна бути з принцом.
 
-let love  = (popelushkaArr, men) => {
-    for (const item of popelushkaArr) {
-        if (item.foot_size === men.shoe) {
-            return ` ${item.name}`
-        }
-    }
-};
+// let love  = (popelushkaArr, men) => {
+//     for (const item of popelushkaArr) {
+//         if (item.foot_size === men.shoe) {
+//             return ` ${item.name}`
+//         }
+//     }
+// };
 // console.log(love(popelushkaArr, men))
 
 //     Додатково, знайти необхідну попелюшку за допомоги функції масиву find та відповідного колбеку
-let love2 = popelushkaArr.find((item) => item.foot_size === men.shoe);
- console.log(love2)
+// let love2 = popelushkaArr.find((item) => item.foot_size === men.shoe);
+//  console.log(love2)
